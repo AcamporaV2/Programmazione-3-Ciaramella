30c30bf3cef79d42869c7a3c4f7c8478ba60007f
Acampora <92322511+AcamporaV2@users.noreply.github.com>
2023-10-16 11:17:11 +0200

    Add files via upload

---
+++ coin.java
@@ -0,0 +1,35 @@
+public class coin {
+    private double value;
+    private String name;
+
+    
+    public coin(String coinName, double coinValue )
+    {
+        value = coinValue;
+        name = coinName;
+    }
+
+
+    public String getName(){
+        return name;
+    }
+    public double getValue(){
+     return value;
+    }
+
+   public boolean sonoUguali(Object moneta1)
+   {
+        coin moneta2 = (coin)moneta1;
+
+        if(this.value == moneta2.value && this.name.equals(moneta2))
+        {
+            System.out.println("argh");
+            return true;
+        }
+        else
+        {
+            System.out.println("hgra");
+            return false;
+        }
+   }
+}
